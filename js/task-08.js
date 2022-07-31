0fe786816faf4d34d9195fb263ac28d8bf74c8d8
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-07-31 14:59:35 +0300

    Upgrade

--- js/task-08.js
+++ js/task-08.js
@@ -4,40 +4,49 @@ const destroyBtn = document.querySelector('[data-action="destroy"]');
 const boxes = document.querySelector('#boxes');
 const input = document.querySelector('#controls > input');
 
-const destroyBoxes = function () {
+renderBtn.addEventListener('click', showInputValues)
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes () {
   const divClear = document.querySelectorAll('#boxes div');
   for (let i = 0; i < divClear.length; i += 1) {
     divClear[i].remove();
   }
 }
 
-renderBtn.addEventListener('click', showInputValues)
-destroyBtn.addEventListener('click', destroyBoxes);
-
 function showInputValues() {
   createBoxes(input.value);
 }
+
+function getRandomColor() {
+  const r = () => Math.random() * (255 - 1) + 1;
+  return `rgb(${r()}, ${r()}, ${r()})`;
+}
+
+const size = 10;
+
+function setBoxSize(number) {
+  let arr = [size];
+
+  for (let i = 0; i < 99; i += 1) {
+    number += 10;
+    arr.push(number);
+  }
+  return arr;
+}
+
+const proportions = setBoxSize(size);
 
 const createBoxes = function (amount) {
   for (let i = 0; i < amount; i += 1) {
     const divAdd = document.createElement('div');
     divAdd.classList.add('design');
-    divAdd.style.width = widths[i];
-    divAdd.style.height = heights[i];
+    divAdd.style.width = proportions[i] + 'px';
+    divAdd.style.height = proportions[i] + 'px';
     divAdd.style.backgroundColor = getRandomColor();
     boxes.appendChild(divAdd);
   }
 }
-
-const getRandomColor = function () {
-  const r = () => Math.random() * (255 - 1) + 1;
-  return `rgb(${r()}, ${r()}, ${r()})`;
-}
-
-
-const widths = ['30px', '40px', '50px', '60px', '70px', '80px', '90px', '100px'];
-const heights = ['30px', '40px', '50px', '60px', '70px', '80px', '90px', '100px'];
-
 
 
 
